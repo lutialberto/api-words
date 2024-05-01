e819364b5d9738cfa8c79ffe79d42626ed9ba7c5
Alberto Luti <betomdq.93@gmail.com>
2024-05-01 14:43:06 -0300

    Feat: added more words and a SP to add correctly formatted words

---
+++ Words.DataBase/dbo/Stored Procedures/Word_Insert.sql
@@ -0,0 +1,32 @@
+﻿CREATE PROCEDURE Word_Insert 
+	@Value VARCHAR(50), 
+	@SimpleValue VARCHAR(50) = NULL
+AS
+BEGIN
+	DECLARE @FormattedValue VARCHAR(50) = TRIM(LOWER(@Value));
+
+	IF(NOT EXISTS(
+		SELECT 1 
+		FROM Word 
+		WHERE [Value] = @FormattedValue
+	)) 
+	BEGIN
+		DECLARE @FormattedSimpleValue VARCHAR(50) = TRIM(LOWER((ISNULL(@SimpleValue,@FormattedValue))))
+		SET @FormattedSimpleValue = REPLACE(@FormattedSimpleValue,'á','a')
+		SET @FormattedSimpleValue = REPLACE(@FormattedSimpleValue,'é','e')
+		SET @FormattedSimpleValue = REPLACE(@FormattedSimpleValue,'í','i')
+		SET @FormattedSimpleValue = REPLACE(@FormattedSimpleValue,'ó','o')
+		SET @FormattedSimpleValue = REPLACE(@FormattedSimpleValue,'ú','u')
+		SET @FormattedSimpleValue = REPLACE(@FormattedSimpleValue,'ü','u')
+
+		INSERT INTO Word (
+			[Value],
+			SimpleValue
+		) 
+		VALUES(
+			@FormattedValue, 
+			@FormattedSimpleValue
+		)
+	END
+END
+GO
