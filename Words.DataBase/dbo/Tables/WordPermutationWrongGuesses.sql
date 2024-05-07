c89075786dc00b9efd580abf63726f5359bb27f0
Alberto Luti <betomdq.93@gmail.com>
2024-05-07 20:12:54 -0300

    Feat: set WordPermutationWorngGuesses table PK

--- Words.DataBase/dbo/Tables/WordPermutationWrongGuesses.sql
+++ Words.DataBase/dbo/Tables/WordPermutationWrongGuesses.sql
@@ -1,6 +1,9 @@
 ﻿CREATE TABLE [dbo].[WordPermutationWrongGuesses] (
-    [Id]	       INT IDENTITY(1,1) NOT NULL,
+    [Id]           INT           IDENTITY (1, 1) NOT NULL,
     [WrongGuesses] VARCHAR (250) NOT NULL,
-    [Letters]      VARCHAR (100) NOT NULL
+    [Letters]      VARCHAR (100) NOT NULL,
+    CONSTRAINT [PK_WordPermutationWrongGuesses] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
