2da50cd8b55d5af8dce1a124d501d980d80a7b9c
Alberto Luti <betomdq.93@gmail.com>
2025-05-24 11:23:44 -0300

    Feat: update word insert sp

---
+++ Words.DataBase/dbo/Functions/CleanWord.sql
@@ -0,0 +1,18 @@
+﻿CREATE FUNCTION dbo.CleanWord (
+    @word NVARCHAR(100)
+)
+RETURNS NVARCHAR(100)
+AS
+BEGIN
+    DECLARE @pos INT, @result NVARCHAR(100)
+
+    -- Buscar la posición del primer "/"
+    SET @pos = CHARINDEX('/', @word)
+
+    -- Si hay "/", cortar hasta esa posición - 1
+    IF @pos > 0
+        SET @result = LEFT(@word, @pos - 1)
+    ELSE
+		SET @result = @word
+	RETURN @result
+END
--- Words.DataBase/dbo/Stored Procedures/Word_Insert.sql
+++ Words.DataBase/dbo/Stored Procedures/Word_Insert.sql
@@ -1,9 +1,11 @@
-﻿CREATE PROCEDURE Word_Insert 
+﻿CREATE PROCEDURE [dbo].[Word_Insert] 
 	@Value VARCHAR(50), 
 	@SimpleValue VARCHAR(50) = NULL
 AS
 BEGIN
-	DECLARE @FormattedValue VARCHAR(50) = TRIM(LOWER(@Value));
+	DECLARE @CleanValue VARCHAR(50) = dbo.CleanWord(@Value);
+	DECLARE @CleanSimpleValue VARCHAR(50) = dbo.CleanWord(@SimpleValue);
+	DECLARE @FormattedValue VARCHAR(50) = TRIM(LOWER(@CleanValue));
 
 	IF(NOT EXISTS(
 		SELECT 1 
@@ -11,7 +13,8 @@ BEGIN
 		WHERE [Value] = @FormattedValue
 	)) 
 	BEGIN
-		DECLARE @FormattedSimpleValue VARCHAR(50) = TRIM(LOWER((ISNULL(@SimpleValue,@FormattedValue))))
+		DECLARE @FormattedSimpleValue VARCHAR(50) = TRIM(LOWER((ISNULL(@CleanSimpleValue,@FormattedValue))))
+
 		SET @FormattedSimpleValue = REPLACE(@FormattedSimpleValue,'á','a')
 		SET @FormattedSimpleValue = REPLACE(@FormattedSimpleValue,'é','e')
 		SET @FormattedSimpleValue = REPLACE(@FormattedSimpleValue,'í','i')
